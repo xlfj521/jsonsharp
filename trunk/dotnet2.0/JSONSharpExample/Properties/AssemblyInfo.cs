1c2ea3e0967fe8f74dffa2c8f1b9b49c3bfda1de
jeff.rodenburg <jeff.rodenburg@736bcbcb-f330-0410-86d2-bb56f7db3b4a>
2007-05-22 06:32:11 +0000

    Relocating example dotnet2.0 project

---
+++ trunk/dotnet2.0/JSONSharpExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JSONSharpExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Judy's Book")]
+[assembly: AssemblyProduct("JSONSharpExample")]
+[assembly: AssemblyCopyright("Copyright © Judy's Book 2007")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("040dffb9-736a-48a0-828f-69554d64b38a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
